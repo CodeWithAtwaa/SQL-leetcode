8ab33a8467d859626aadafce84542942b07bdbf9
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-09-15 21:21:24 +0300

    1211. Queries Quality and Percentage

---
+++ 1211. Queries Quality and Percentage.sql
@@ -0,0 +1,16 @@
+--  1211. Queries Quality and Percentage
+
+
+SELECT 
+    query_name,
+    ROUND(AVG(rating / position),2)  as quality,
+    ROUND(
+        SUM(
+            CASE
+             WHEN rating < 3 THEN 1
+             ELSE 0 
+            END) * 100.0 / COUNT(*), 2
+    ) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name
