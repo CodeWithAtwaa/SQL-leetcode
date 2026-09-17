d43b7072f0a287b0afd533429460bcfe0c794508
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-09-17 20:22:14 +0300

    SQL

---
+++ 1484. Group Sold Products By The Date.sql
@@ -0,0 +1,9 @@
+--  1484. Group Sold Products By The Date
+
+SELECT 
+    sell_date,
+    COUNT(DISTINCT product) num_sold,
+    GROUP_CONCAT(DISTINCT product ORDER BY product ASC SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date ASC
