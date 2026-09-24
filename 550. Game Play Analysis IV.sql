7f3a54bc38794787ab7370537197106917b751ab
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-09-25 00:16:07 +0300

    SQL

---
+++ 550. Game Play Analysis IV.sql
@@ -0,0 +1,25 @@
+--  550. Game Play Analysis IV
+
+# Write your MySQL query statement below
+
+WITH cte AS (
+    SELECT 
+        player_id, 
+        MIN(event_date) AS first_date
+    FROM 
+        Activity
+    GROUP BY 
+        player_id
+)
+SELECT 
+    ROUND(
+        COUNT(DISTINCT a.player_id) / (SELECT COUNT(DISTINCT player_id) FROM Activity), 
+        2
+    ) AS fraction
+FROM 
+    cte fl
+JOIN 
+    Activity a 
+ON 
+    fl.player_id = a.player_id 
+    AND a.event_date = DATE_ADD(fl.first_date, INTERVAL 1 DAY);
