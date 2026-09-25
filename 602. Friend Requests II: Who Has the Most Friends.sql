93d8e86e58736674edeb16b682559ba4c78c7e6b
CodeWithAtwaa <mohamedatwaa341@gmail.com>
2026-09-25 17:57:09 +0300

    END SQL 50

---
+++ 602. Friend Requests II: Who Has the Most Friends.sql
@@ -0,0 +1,10 @@
+-- 602. Friend Requests II: Who Has the Most Friends
+
+# Write your MySQL query statement below
+
+select requester_id as id , count(requester_id) as num
+from (select requester_id from RequestAccepted 
+union all
+select accepter_id from RequestAccepted) as temp
+group by id
+order by num desc limit 1 ;
